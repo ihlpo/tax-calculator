5cc50ba19fba1db1558983359aebe36db098f550
Ivan <21068310+ihlpo@users.noreply.github.com>
2021-01-20 23:16:44 +1100

    Added tax % feature

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ function calculate_salaries(salary){
    annuallyPay.innerHTML = currencyFormat(year);
 }
 
+function calculate_percentage(salary, tax){
+   return (tax / salary);
+}
+
 function calculate_tax(){
    let salary = document.getElementById("payinput").value;
    let output = document.getElementById("taxoutput");
    let salary_frequecy = document.getElementById("payfreq").value;
+   let tax_percentage = document.getElementById("percent");
 
       
    if (salary < 0){
@@ -41,19 +46,24 @@ function calculate_tax(){
    else if (salary >= 18201 && salary <= 45000){
       let total = (salary - 18200) * 0.19;
       output.innerHTML = currencyFormat(total);
+      tax_percentage.innerHTML = `${((total/salary) * 100).toFixed(2)}%`;
    }
    else if (salary >= 45001 && salary <= 120000){
       let total = ((salary - 45000) * 0.325) + 5092;
       output.innerHTML = currencyFormat(total);
+      tax_percentage.innerHTML = `${((total/salary) * 100).toFixed(2)}%`;
    }
    else if (salary >= 120001 && salary <= 180000){
       let total = ((salary - 120000) * 0.37) + 29467; 
       output.innerHTML = currencyFormat(total);
+      tax_percentage.innerHTML = `${((total/salary) * 100).toFixed(2)}%`;
    }
    else{
       let total = ((salary - 180000) * 0.45) + 51667;
       output.innerHTML = currencyFormat(total);
+      tax_percentage.innerHTML = `${((total/salary) * 100).toFixed(2)}%`;
    }
+
 }
 
 
